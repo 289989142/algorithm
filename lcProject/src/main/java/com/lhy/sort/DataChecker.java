d4cb5be459a258e8c7ddf083cc11e774e0a7f9d9
289989142 <13941715477@163.com>
2021-03-05 16:41:14 +0800

    选择排序优化

---
+++ lcProject/src/main/java/com/lhy/sort/DataChecker.java
@@ -0,0 +1,48 @@
+package com.lhy.sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @ClassName DataChecker
+ * @Description 对数器
+ * @Author lihengyu
+ * @Date 2021/3/5 16:22
+ * @Version 1.0
+ */
+public class DataChecker {
+    static int[] generateRandomArray(){
+        Random r = new Random();
+
+        int[] arr = new int[10000];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt();
+        }
+
+        return arr;
+    }
+
+    static void check(){
+        int[] arr = generateRandomArray();
+        int[] arrCopy = new int[arr.length];
+        System.arraycopy(arr,0,arrCopy,0,arr.length);
+
+        Arrays.sort(arr);
+        SelectionSort.sort(arrCopy);
+
+        boolean same = true;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != arrCopy[i]) {
+                same = false;
+                break;
+            }
+        }
+
+        System.out.println(same ? "right" : "wrong");
+    }
+
+    public static void main(String[] args) {
+        check();
+    }
+}
--- lcProject/src/main/java/com/lhy/sort/SelectionSort.java
+++ lcProject/src/main/java/com/lhy/sort/SelectionSort.java
@@ -2,36 +2,43 @@ package com.lhy.sort;
 
 /**
  * @ClassName SelectionSort
- * @Description 选择排序
+ * @Description 选择排序 时间O(n2)  空间O(1)  不稳定
  * @Author lihengyu
  * @Date 2021/3/5 15:36
  * @Version 1.0
  */
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] arr= {11,2,3,123,5};
-        int[] sorted = SelectionSort.selection(arr);
-
-        for (int value : sorted) {
-            System.out.print(value+" ");
-        }
+        int[] arr= {11,2,3,123,5,5,1,3,5};
+        SelectionSort.sort(arr);
+        printArr(arr);
     }
-    public static int[] selection(int[] arr){
+    public static int[] sort(int[] arr){
         int length = arr.length;
         int minPos = 0;
+
         for (int i = 0; i < length; i++) {
+            minPos = i;
+
             for (int j = i; j < length; j++) {
-                if (arr[j]<arr[minPos]){
-                    minPos = j;
-                }
+                minPos = arr[j]<=arr[minPos] ? j : minPos;
             }
-            if (minPos != i){
-                int swap = arr[minPos];
-                arr[minPos] = arr[i];
-                arr[i] = swap;
-            }
+
+            swap(arr,minPos,i);
         }
 
         return arr;
+    }
+
+    public static void printArr(int[] arr){
+        for (int value : arr) {
+            System.out.print(value+" ");
+        }
+    }
+
+    public static void swap(int[] arr,int pos1,int pos2){
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
     }
 }
